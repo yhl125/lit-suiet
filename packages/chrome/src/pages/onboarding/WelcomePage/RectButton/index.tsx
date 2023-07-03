34be84587dc23ad53dd57fb2bd451006c1233fe4
minseokkim <altpd13@gmail.com>
2023-07-03 20:38:57 +0900

    Fix styles for rect button and welcome page

--- packages/chrome/src/pages/onboarding/WelcomePage/RectButton/index.tsx
+++ packages/chrome/src/pages/onboarding/WelcomePage/RectButton/index.tsx
@@ -3,35 +3,17 @@ import styles from './index.module.scss';
 import { Extendable } from '../../../../types';
 import { ReactComponent as PlusPrimary } from '../../../../assets/icons/plus-primary.svg';
 import { ReactComponent as PlusSecondary } from '../../../../assets/icons/plus-secondary.svg';
-import { ReactComponent as FingerPrint } from '../../../../assets/icons/fingerprint.svg';
 import Icon from '../../../../components/Icon';
 
 export type RectButtonProps = Extendable & {
-  theme?: 'primary' | 'default' | 'biometric' | 'biometric-secondary';
+  theme?: 'primary' | 'default';
   to?: string;
   onClick?: () => void;
 };
 
 const RectButton = (props: RectButtonProps) => {
   const { theme = 'default' } = props;
-  let icon;
-  if (theme === 'primary') {
-    icon = <Icon icon={<PlusPrimary />}></Icon>;
-  } else if (theme === 'biometric') {
-    icon = (
-      <span className={styles['create-biometric__logo']}>
-        <FingerPrint width="24" height="24" />
-      </span>
-    );
-  } else if (theme === 'biometric-secondary') {
-    icon = (
-      <span className={styles['signin-biometric__logo']}>
-        <FingerPrint width="24" height="24" />
-      </span>
-    );
-  } else {
-    icon = <Icon icon={<PlusSecondary />} />;
-  }
+
   return (
     <button
       onClick={props.onClick}
@@ -41,7 +23,11 @@ const RectButton = (props: RectButtonProps) => {
         props.className
       )}
     >
-      {icon}
+      {theme === 'primary' ? (
+        <Icon icon={<PlusPrimary />} />
+      ) : (
+        <Icon icon={<PlusSecondary />} />
+      )}
       {props.children}
     </button>
   );
